9e54d64005d13844a8b4e9aec706acb0e15951a4
Bishoy Tawfik <33475254+Bishoy-Tawfik@users.noreply.github.com>
2025-02-28 14:01:14 -0800

    Adding my eda file

---
+++ notebooks/eda/eda-utils.py
@@ -0,0 +1,86 @@
+import json
+
+
+def count_objects_with_same_labels(json_file, label):
+    with open(json_file, 'r', encoding='utf-8') as file:
+        data = json.load(file)
+    
+    count = 0
+    for obj in data.values():
+        if 'labels_task3_1' in obj and all(l.upper() == label for l in obj['labels_task3_1']):
+            count += 1
+    
+    return count
+
+
+def count_objects_in_language(json_file, lang):
+    with open(json_file, 'r', encoding='utf-8') as file:
+        data = json.load(file)
+    
+    count = 0
+    for obj in data.values():
+        if 'lang' in obj and obj['lang'] == lang:
+            count += 1
+    
+    return count
+
+
+def get_annotator_lst(json_file):
+    with open(json_file, 'r', encoding='utf-8') as file:
+        data = json.load(file)
+
+    annotators = []
+    for obj in data.values():
+        if 'annotators' in obj:
+            for a in obj['annotators']:
+                if not a in annotators:
+                    annotators.append(a)
+    return annotators
+
+
+def get_classifications_lst(json_file):
+    with open(json_file, 'r', encoding='utf-8') as file:
+        data = json.load(file)
+
+    classifications = []
+    for obj in data.values():
+        if 'labels_task3_3' in obj:
+            for c in obj['labels_task3_3']:
+                if not c in classifications:
+                    classifications.append(c)
+    return classifications
+
+
+def get_types_lst(json_file):
+    with open(json_file, 'r', encoding='utf-8') as file:
+        data = json.load(file)
+
+    types = []
+    for obj in data.values():
+        if 'labels_task3_2' in obj:
+            for t in obj['labels_task3_2']:
+                if not t in types:
+                    types.append(t)
+    return types
+
+
+def main():
+    #Update the file location below before running
+    json_file = "C:\\Users\\bisho\\Downloads\\EXIST 2025 Dataset V0.1_Extract\\EXIST 2025 Dataset V0.1 (3)\\EXIST 2025 Videos Dataset\\training\\EXIST2025_training.json"
+    label_val = "YES"
+    result = count_objects_with_same_labels(json_file, label_val)
+    print(f"Number of objects with all {label_val} labels in 'labels_task3_1':  {result}")
+    label_val_no = "NO"
+    result_no = count_objects_with_same_labels(json_file, label_val_no)
+    print(f"Number of objects with all {label_val_no} labels in 'labels_task3_1':  {result_no}")
+    lang= "en"
+    result_lang = count_objects_in_language(json_file, lang)
+    print(f"Number of objects in {lang}: {result_lang}")
+    print(f"Annotators: {get_annotator_lst(json_file)}")
+    classifications = get_classifications_lst(json_file)
+    print(f"Classifications: {classifications}")
+    print(f"Classifications count: {len(classifications)}")
+    print(f"types: {get_types_lst(json_file)}")
+
+if __name__ == "__main__":
+    main()
